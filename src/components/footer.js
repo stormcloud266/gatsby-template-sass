0ef9804fc1edd875da72831dc188e13f52325075
Tawnee Allport <allprt89@gmail.com>
2020-09-17 12:48:02 -0600

    add minimal footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,40 @@
 import React from "react"
+import { Link } from "gatsby"
+
+import { Logo } from '../images/icons'
 
 const Footer = () => (
-  <footer>
-    footer
+  <footer className="bg-dark footer">
+    <div className="wrapper footer__inner">
+      
+      <Logo />
+
+      <div className="footer__text">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In obcaecati vitae consectetur.</p>
+
+        <nav className="footer__nav">
+          <ul className="footer__nav-list">
+            <li>
+              <Link to="/">Page</Link>
+            </li>
+            <li>
+              <Link to="/">Page</Link>
+            </li>
+            <li>
+              <Link to="/">Page</Link>
+            </li>
+            <li>
+              <Link to="/">Page</Link>
+            </li>
+            <li>
+              <Link to="/">Page</Link>
+            </li>
+          </ul>
+        </nav>
+
+      </div>
+
+    </div>
   </footer>
 )
 
